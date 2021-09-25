2c7b252e10944938e898db3ceaa168ec18d0f7f5
ChalanthornA <chalanthorn.a@ku.th>
2021-09-26 00:11:28 +0700

    add message when token expires

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,3 +1,12 @@
 import { AuthGuard } from "@nestjs/passport";
+import { TokenExpiredError } from "jsonwebtoken";
+import { UnauthorizedException } from "@nestjs/common";
 
-export class JwtAuthGuard extends AuthGuard('accessToken'){}
+export class JwtAuthGuard extends AuthGuard('accessToken'){
+    handleRequest(err, user, info:Error){
+        if (info instanceof TokenExpiredError){
+            throw new UnauthorizedException('token expired');
+        }
+        return user;
+    }
+}
