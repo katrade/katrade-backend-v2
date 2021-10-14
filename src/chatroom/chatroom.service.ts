2e778319a0728dc82b60dd6d3a53f735ab88a56a
RyuohNagito <bonjapan1234@gmail.com>
2021-10-14 22:56:39 +0700

    fix lastMessage null Error 2

--- src/chatroom/chatroom.service.ts
+++ src/chatroom/chatroom.service.ts
@@ -45,7 +45,7 @@ export class ChatroomService {
     async LastMessage(roomid: string){
         let chatroom = await this.chatroomModel.findOne({roomId: roomid});
         if (!chatroom)
-            return true;
+            return [];
         let idx = chatroom.messages.length;
         //console.log(chatroom);
         if (idx == 0) {
